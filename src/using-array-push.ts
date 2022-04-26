2882c0aec029a26362dca0b97addeff55b3eafb0
Monknow <juanrodriguezcode@gmail.com>
2022-04-26 19:03:50 -0400

    new branch using push only

--- src/using-array-push.ts
+++
@@ -1,6 +0,0 @@
-import {Dish, Comment} from "./interfaces";
-
-export const addNewComment = (dish: Dish, newComment: Comment) => {
-	dish.comments.push(newComment);
-	console.log(dish.comments);
-};
