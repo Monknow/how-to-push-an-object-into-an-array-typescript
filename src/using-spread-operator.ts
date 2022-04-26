bafec479d4f1504100615a6211497ad9aeb9cb9e
Monknow <juanrodriguezcode@gmail.com>
2022-04-26 19:00:26 -0400

    new branch using push only

--- src/using-spread-operator.ts
+++
@@ -1,8 +0,0 @@
-import {Dish, Comment} from "./interfaces";
-
-export const addNewComment = (dish: Dish, newComment: Comment) => {
-	const {comments} = dish;
-
-	dish.comments = [...comments, newComment];
-	console.log(dish.comments);
-};
