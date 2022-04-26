f80dc45d1535b99ffa3add0f4ce8ea6fe58eddd2
Monknow <juanrodriguezcode@gmail.com>
2022-04-26 18:58:52 -0400

    new branch with concat only

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
+import {Dish, Comment} from "./interfaces";
 import {pastaDish, pastaComment} from "./pastaDish";
-import {addNewComment as addNewCommentUsingPush} from "./using-array-push";
-import {addNewComment as addNewCommentUsingConcat} from "./using-array-concat";
-import {addNewComment as addNewCommentUsingSpread} from "./using-spread-operator";
-import {addNewComment as addNewCommentUsingSpreadNoMutations} from "./using-spread-operator-and-no-mutations";
 
-// Methods that mutate the original object
-// Therefore, other functions' output will be affected by them
+const addNewComment = (dish: Dish, newComment: Comment) => {
+	const {comments} = dish;
 
-addNewCommentUsingPush(pastaDish, pastaComment);
-addNewCommentUsingSpread(pastaDish, pastaComment);
+	const editedDish = {...dish, comments: [...comments, newComment]};
+	console.log(editedDish.comments);
 
-// Methods that don't mutate the original object, but create a new one
-addNewCommentUsingConcat(pastaDish, pastaComment);
-addNewCommentUsingSpreadNoMutations(pastaDish, pastaComment);
+	return editedDish;
+};
 
-// In this case, addNewCommentUsingPush and addNewCommentUsingSpread add one comment each to pastaDish
-// so pastaDish will have two comments
-// Then addNewCommentUsingConcat and addNewCommentUsingSpreadNoMutations will output three comments
-// But they won't mutate pastaDish, so it will remain with two comments
+addNewComment(pastaDish, pastaComment);
