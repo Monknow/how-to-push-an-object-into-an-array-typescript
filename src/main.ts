c38fc148adcd644a20fb2b94b7c3de014c669bf9
Monknow <juanrodriguezcode@gmail.com>
2022-04-27 20:43:58 -0400

    actually added the concat function; it was using spread instead

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import {Dish, Comment} from "./interfaces";
 import {pastaDish, pastaComment} from "./pastaDish";
 
 const addNewComment = (dish: Dish, newComment: Comment) => {
-	const {comments} = dish;
-
-	const editedDish = {...dish, comments: [...comments, newComment]};
+	const editedComments = dish.comments.concat(newComment);
+	const editedDish = Object.assign({}, dish, {comments: editedComments});
 	console.log(editedDish.comments);
 
 	return editedDish;
